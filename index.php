26afafd7867e747739e24911b6c0d037240bff52
BasilBosnjak <basil.bosnjak@gmail.com>
2022-03-16 10:55:12 +0100

    Connecting to database

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 require 'vendor/autoload.php';
+require_once("rest/dao/ProjectDao.class.php");
 
 Flight::route('/', function() {
+
+  $dao = new ProjectDao();
+  $users = $dao->get_all();
+  print_r($users);
     echo "Creating a new branch!";
 });
 
